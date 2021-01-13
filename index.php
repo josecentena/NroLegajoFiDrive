fe160b604843372f41e91057835e83341092085f
josecentena <tonycipo50@gmail.com>
2021-01-13 16:36:55 -0300

    Segundo commit

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-
+echo " Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.";
+echo " Mis nombres es: José Antonio Centena";
+echo " Mi Nro de Legajo es: 88534";
 
 ?>
